e1f6355abe6e11d077189b98f645f4ba47088c65
Michel Pomares <mapomarest@gmail.com>
2023-01-27 13:48:30 +0100

    Small typo mistake refractor

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -12,8 +12,8 @@ function  AboutPage () {
             <h1>About:</h1>
             <p>
             This is a MERN app for teachers to offer services which students can book <br />
-            You can create a profile either as a teacher or student.Teachers can create services or book <br />
-            another teacher services. Students can only book services. 
+            You can create a profile either as a teacher or student. Teachers can create services or book <br />
+            another teacher's services. Students can only book the services offered. 
             </p>
              <p>Created by: <a href="https://www.linkedin.com/in/michel-pomares/">Michel Pomares</a></p>   
              <Image src={picture} fluid />
